194dbcd1e2b9a65b52085ad061c78ef9f7675931
Gurveen <gurveens11@gmail.com>
2021-09-30 16:45:30 +0530

    Create main and footer section

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 import React from 'react'
-import { Navbar } from './components'
-import { Layout, Typography, Space } from 'antd'
 import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
-const App = () => {
-	return (
-		<div className="app">
-			<Navbar className="navbar" />
-			<div className="main">
-				<Layout>
-					<div className="routes">
-						<Switch>
-							<Route path="/" exact></Route>
-							<Route path="/cryptocurrencies" exact></Route>
-							<Route path="/crypto/:id" exact></Route>
-							<Route path="/exchanges" exact></Route>
-							<Route path="/news" exact></Route>
-						</Switch>
-					</div>
-				</Layout>
-				<div className="footer">
-					<Typography.Title
-						level={5}
-						style={{ color: 'white', textAlign: 'center' }}
-					>
-						Copyright © {new Date().getFullYear()}
-						<Link to="/">CryptoIdeas Inc.</Link> <br />
-						All Rights Reserved.
-					</Typography.Title>
-					<Space>
-						<Link to="/">Home</Link>
-						<Link to="/exchanges">Exchanges</Link>
-						<Link to="/news">News</Link>
-					</Space>
+import {
+	Exchanges,
+	Homepage,
+	News,
+	Cryptocurrencies,
+	CryptoDetails,
+	Navbar,
+} from './components'
+import './App.css'
+
+const App = () => (
+	<div className="app">
+		<div className="navbar">
+			<Navbar />
+		</div>
+		<div className="main">
+			<Layout>
+				<div className="routes">
+					<Switch>
+						<Route exact path="/">
+							<Homepage />
+						</Route>
+						<Route exact path="/exchanges">
+							<Exchanges />
+						</Route>
+						<Route exact path="/cryptocurrencies">
+							<Cryptocurrencies />
+						</Route>
+						<Route exact path="/crypto/:coinId">
+							<CryptoDetails />
+						</Route>
+						<Route exact path="/news">
+							<News />
+						</Route>
+					</Switch>
 				</div>
+			</Layout>
+			<div className="footer">
+				<Typography.Title
+					level={5}
+					style={{ color: 'white', textAlign: 'center' }}
+				>
+					Copyright © 2021 | <Link to="/">CryptoIdeas Inc.</Link> <br />
+					All Rights Reserved.
+				</Typography.Title>
+				<Space>
+					<Link to="/">Home</Link>
+					<Link to="/exchanges">Exchanges</Link>
+					<Link to="/news">News</Link>
+				</Space>
 			</div>
 		</div>
-	)
-}
+	</div>
+)
 
 export default App
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Typography, Row, Col, Statistic } from 'antd'
+
+const { Title } = Typography
+const Homepage = () => {
+	return (
+		<>
+			<Title level={2} className="heading">
+				Global Crypto Statistics
+			</Title>
+			<Row>
+				<Col span={12}>
+					<Statistic title="Total Cryptocurrencies" value={5}></Statistic>
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total Exchanges" value={5}></Statistic>
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total Market Cap" value={5}></Statistic>
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total 24h Volume" value={5}></Statistic>
+				</Col>
+				<Col span={12}>
+					<Statistic title="Total Markets" value={5}></Statistic>
+				</Col>
+			</Row>
+		</>
+	)
+}
+
+export default Homepage
