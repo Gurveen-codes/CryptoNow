05b6e49aca35de85a89aadd5b8c640f6a0065cc8
Gurveen <gurveens11@gmail.com>
2021-09-30 17:00:33 +0530

    Create Homepage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,1 +1,32 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const apiHeaders = {
+	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	'x-rapidapi-key': 'e9e6a1f59emsh79c7deec9121014p137123jsn18eaa88031e0',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({
+	url,
+	headers: apiHeaders,
+})
+
+export const cryptoApi = createApi({
+	reducerPath: 'cryptoApi',
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: () => createRequest('/exchanges'),
+		}),
+	}),
+})
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'e9e6a1f59emsh79c7deec9121014p137123jsn18eaa88031e0'
+//     }
+//   };
