c64c053a3bffa3f39f410ed8b7af0df291d95fe6
Gurveen <gurveens11@gmail.com>
2021-10-01 17:25:39 +0530

    Create CryptoDetails page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -14,13 +14,12 @@ import {
 	NumberOutlined,
 	ThunderboltOutlined,
 } from '@ant-design/icons'
-
 import {
 	useGetCryptoDetailsQuery,
 	useGetCryptoHistoryQuery,
 } from '../services/cryptoApi'
-// import Loader from './Loader';
-// import LineChart from './LineChart';
+
+import LineChart from './LineChart'
 
 const { Title, Text } = Typography
 const { Option } = Select
@@ -29,69 +28,74 @@ const CryptoDetails = () => {
 	const { coinId } = useParams()
 	const [timeperiod, setTimeperiod] = useState('7d')
 	const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
-	//   const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod });
+	const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod })
+
 	const cryptoDetails = data?.data?.coin
 
-	//   if (isFetching) return <Loader />;
-
 	const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y']
+	let stats = []
+	let genericStats = []
+	if (!isFetching) {
+		stats = [
+			{
+				title: 'Price to USD',
+				value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`,
+				icon: <DollarCircleOutlined />,
+			},
+			{ title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+			{
+				title: '24h Volume',
+				value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
+				icon: <ThunderboltOutlined />,
+			},
+			{
+				title: 'Market Cap',
+				value: `$ ${
+					cryptoDetails.marketCap && millify(cryptoDetails.marketCap)
+				}`,
+				icon: <DollarCircleOutlined />,
+			},
+			{
+				title: 'All-time-high(daily avg.)',
+				value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
+				icon: <TrophyOutlined />,
+			},
+		]
 
-	const stats = [
-		{
-			title: 'Price to USD',
-			value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`,
-			icon: <DollarCircleOutlined />,
-		},
-		{ title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-		{
-			title: '24h Volume',
-			value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
-			icon: <ThunderboltOutlined />,
-		},
-		{
-			title: 'Market Cap',
-			value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`,
-			icon: <DollarCircleOutlined />,
-		},
-		{
-			title: 'All-time-high(daily avg.)',
-			value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
-			icon: <TrophyOutlined />,
-		},
-	]
+		genericStats = [
+			{
+				title: 'Number Of Markets',
+				value: cryptoDetails.numberOfMarkets,
+				icon: <FundOutlined />,
+			},
+			{
+				title: 'Number Of Exchanges',
+				value: cryptoDetails.numberOfExchanges,
+				icon: <MoneyCollectOutlined />,
+			},
+			{
+				title: 'Aprroved Supply',
+				value: cryptoDetails.approvedSupply ? (
+					<CheckOutlined />
+				) : (
+					<StopOutlined />
+				),
+				icon: <ExclamationCircleOutlined />,
+			},
+			{
+				title: 'Total Supply',
+				value: `$ ${millify(cryptoDetails.totalSupply)}`,
+				icon: <ExclamationCircleOutlined />,
+			},
+			{
+				title: 'Circulating Supply',
+				value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
+				icon: <ExclamationCircleOutlined />,
+			},
+		]
+	}
 
-	const genericStats = [
-		{
-			title: 'Number Of Markets',
-			value: cryptoDetails.numberOfMarkets,
-			icon: <FundOutlined />,
-		},
-		{
-			title: 'Number Of Exchanges',
-			value: cryptoDetails.numberOfExchanges,
-			icon: <MoneyCollectOutlined />,
-		},
-		{
-			title: 'Aprroved Supply',
-			value: cryptoDetails.approvedSupply ? (
-				<CheckOutlined />
-			) : (
-				<StopOutlined />
-			),
-			icon: <ExclamationCircleOutlined />,
-		},
-		{
-			title: 'Total Supply',
-			value: `$ ${millify(cryptoDetails.totalSupply)}`,
-			icon: <ExclamationCircleOutlined />,
-		},
-		{
-			title: 'Circulating Supply',
-			value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
-			icon: <ExclamationCircleOutlined />,
-		},
-	]
-
+	if (isFetching) return 'Loading...'
 	return (
 		<Col className="coin-detail-container">
 			<Col className="coin-heading-container">
@@ -113,7 +117,12 @@ const CryptoDetails = () => {
 					<Option key={date}>{date}</Option>
 				))}
 			</Select>
-			{/* <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} /> */}
+
+			<LineChart
+				coinHistory={coinHistory}
+				currentPrice={millify(cryptoDetails.price)}
+				coinName={cryptoDetails.name}
+			/>
 			<Col className="stats-container">
 				<Col className="coin-value-statistics">
 					<Col className="coin-value-statistics-heading">
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
-	'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-	'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	'x-rapidapi-key': 'e9e6a1f59emsh79c7deec9121014p137123jsn18eaa88031e0',
 }
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
 	reducerPath: 'cryptoApi',
-	baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+	baseQuery: fetchBaseQuery({ baseUrl }),
 	endpoints: (builder) => ({
 		getCryptos: builder.query({
 			query: (count) => createRequest(`/coins?limit=${count}`),
