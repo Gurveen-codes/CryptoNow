85331c22ddae52fcdddb2e6c20be7f8cac549fd1
Gurveen <gurveens11@gmail.com>
2021-10-01 18:08:14 +0530

    Add Exchanges page

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,62 @@
 import React from 'react'
+import millify from 'millify'
+import { Collapse, Row, Col, Typography, Avatar } from 'antd'
+import HTMLReactParser from 'html-react-parser'
+
+import { useGetExchangesQuery } from '../services/cryptoApi'
+
+const { Text } = Typography
+const { Panel } = Collapse
 
 const Exchanges = () => {
-	return <div>Exchanges</div>
+	const { data, isFetching } = useGetExchangesQuery()
+	const exchangesList = data?.data?.exchanges
+
+	if (isFetching) return 'Loading...'
+
+	return (
+		<>
+			<Row>
+				<Col span={9}>Exchanges</Col>
+				<Col span={5}>24h Trade Volume</Col>
+				<Col span={5}>Markets</Col>
+				<Col span={5}>Change</Col>
+			</Row>
+			<Row>
+				{exchangesList.map((exchange) => (
+					<Col span={24}>
+						<Collapse>
+							<Panel
+								key={exchange.id}
+								showArrow={false}
+								header={
+									<Row key={exchange.id}>
+										<Col span={9}>
+											<Text>
+												<strong>{exchange.rank}.</strong>
+											</Text>
+											<Avatar
+												className="exchange-image"
+												src={exchange.iconUrl}
+											/>
+											<Text>
+												<strong>{exchange.name}</strong>
+											</Text>
+										</Col>
+										<Col span={5}>${millify(exchange.volume)}</Col>
+										<Col span={5}>{millify(exchange.numberOfMarkets)}</Col>
+										<Col span={5}>{millify(exchange.marketShare)}%</Col>
+									</Row>
+								}
+							>
+								{HTMLReactParser(exchange.description || '')}
+							</Panel>
+						</Collapse>
+					</Col>
+				))}
+			</Row>
+		</>
+	)
 }
 
 export default Exchanges
